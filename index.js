232715175a634fdfcdc2d928c006bea0f20f1260
Del <dmanzueta@gmail.com>
2020-12-26 21:43:32 -0500

    Created new branch for 2.5 Task

--- index.js
+++ index.js
@@ -2,9 +2,105 @@ const express = require('express'),
     morgan = require('morgan'),
     app = express(),
     bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    methodOverride = require('method-override'),
+    uuid = require('uuid');
 
+
+let movies = [{
+    id: 1,
+    title: 'The Godfather',
+    genres: {
+        crime: 'Crime',
+        drama: 'Drama'
+    },
+    id: 2,
+    title: 'City of God',
+    genres: {
+        crime: 'Crime',
+        drama: 'Drama'
+    },
+    id: 3,
+    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+    genres: {
+        comedy: 'Comedy'
+    },
+    id: 4,
+    title: 'Her',
+    genres: {
+        drama: 'Drama',
+        romance: 'Romance'
+    },
+    id: 5,
+    title: 'Romeo + Juliet',
+    genres: {
+        drama: 'Drama',
+        romance: 'Romance'
+    },
+    id: 6,
+    title: '12 Angry Men',
+    genres: {
+        crime: 'Crime',
+        drama: 'Drama'
+    },
+    id: 7,
+    title: 'Ad Astra',
+    genres: {
+        adventure: 'Adventure',
+        drama: 'Drama',
+        mystery: 'Mystery'
+    },
+    id: 8,
+    title: '2001: A Space Odyssey',
+    genres: {
+        adventure: 'Adventure',
+        sci_fi: 'Sci-Fi'
+    },
+    id: 9,
+    title: 'V for Vendetta',
+    genres: {
+        action: 'Action',
+        drama: 'Drama',
+        sci_fi: 'Sci-Fi'
+    },
+    id: 10,
+    title: 'Léon: The Professional',
+    genres: {
+        action: 'Action',
+        crime: 'Crime',
+        drama: 'Drama'
+    }
+}];
+
+
+//get list of data about all movies
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+//get data about a single movie by name
+app.get('/movies/:title', (req, res) => {
+    res.json(movies.find((movies) => {
+        return movies.name === req.params.name
+    }));
+});
+
+//add a new movie to the list
+app.post('/movies', (req, res) => {
+    let newMovie = req.body;
+    if (!newMovie.name) {
+        const message = 'Missing movie title';
+        res.status(400).send(message);
+    } else {
+        newMovie.id = uuid.v11();
+        movies.push(newMovie);
+        res.status(201).send(newMovie);
+    }
+});
+
+/*
 app.use(morgan('common'));
+// app.use('/', express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to my app!');
@@ -26,9 +122,12 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-// app.use('/', express.static('public'));
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Successful GET request returning data on all the movies');
+});
 
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
+
+*/
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ url = require('url');
 
 http.createServer((request, response) => {
     let requestUrl = url.parse(request.url, true);
-    if (requestUrl.pathname == '/documentation.html') {
+    if (requestUrl.pathname == '/public/documentation.html') {
         response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.end('Documentation on the book club API .\n');
+        response.end('Documentation on the Movie App API .\n');
     } else {
         response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.end('Welcome to my book club! \n');
+        response.end('Welcome to my Movie API \n');
     }
 }).listen(8080);
 
 
-console.log('My first Node test server is running on Port 8080.');
+console.log('Node test server is running on Port 8080.');
