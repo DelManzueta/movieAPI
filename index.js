9fff9446efda07033aaa23a44e3f51716605777f
Del <dmanzueta@gmail.com>
2020-12-30 15:45:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,101 +8,11 @@ const
 const
     app = express(),
     http = require('http'),
-    url = require('url');
+    url = require('url'),
+    uuid = require('uuid');
 
 
-let movies = [{
-        id: 1,
-        title: '12 Angry Men',
-        director: 'Sidney Lumet',
-        mainActors: ['Henry Fonda', 'Lee J. Cobb', 'Martin Balsam'],
-        genre: ['Drama', 'Crime'],
-        year: '1957',
-        collection: null
-    },
-    {
-        id: 2,
-        title: 'Fight Club',
-        director: 'David Fincher',
-        mainActors: ['Brad Pitt', 'Edward Norton', 'Helena Bonham Carter'],
-        genre: 'Drama',
-        year: '1999',
-        collection: null
-    },
-    {
-        id: 3,
-        title: 'American Pop',
-        director: 'Ralph Bakshi',
-        mainActors: 'Ron Thompson',
-        genre: ['Animation', 'Drama'],
-        year: '1981',
-        collection: null
-    },
-    {
-        id: 4,
-        title: 'The Wiz',
-        director: 'Sindey Lumet',
-        mainActors: ['Michael Jackson', 'Diana Ross', 'Nipsey Russell'],
-        genre: ['Adventure', 'Family', 'Fantasy'],
-        year: '1978',
-        collection: null
-    },
-    {
-        id: 5,
-        title: 'Hook',
-        director: 'Steven Spielberg',
-        mainActors: ['Robin Williams', 'Dustin Hoffman', 'Julia Roberts'],
-        genre: ['Adventure', 'Comedy', 'Family'],
-        year: '1991',
-        collection: null
-    },
-    {
-        id: 6,
-        title: 'Dog Day Afternoon',
-        director: 'Sidney Lumet',
-        mainActors: ['Al Pacino', 'John Cazale'],
-        genre: ['Biography', 'Crime', 'Drama'],
-        year: '1975',
-        collection: null
-    },
-    {
-        id: 7,
-        title: 'Ready Player One',
-        director: 'Steven Spielberg',
-        mainActors: ['Tye Sheridan', 'Olivia Cooke', 'Ben Mendelsohn'],
-        genre: ['Action', 'Adventure', 'Sci-Fi'],
-        year: '2018',
-        collection: null
-    },
-    {
-        id: 8,
-        title: 'Rogue One: A Star Wars Story',
-        director: 'Gareth Edwards',
-        mainActors: ['Felicity Jones', 'Diego Luna', 'Ben Mendelsohn'],
-        genre: ['Action', 'Adventure', 'Sci-Fi'],
-        year: '2016',
-        collection: 'Star Wars'
-    },
-    {
-        id: 9,
-        title: 'The Lord of the Rings: The Fellowship of the Ring',
-        director: 'Peter Jackson',
-        author: 'J.R.R Tolkien',
-        mainActors: ['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'],
-        genre: ['Action', 'Adventure', 'Fantasy'],
-        year: '2001',
-        collection: 'The Lord of the Rings'
-    },
-    {
-        id: 10,
-        title: 'Carlito\'s Way',
-        director: 'Brian De Palma',
-        mainActors: ['Al Pacino', 'Sean Penn', 'Viggo Mortensen'],
-        year: '1993',
-        genre: ['Crime', 'Drama', 'Thriller'],
-        collection: null
-    },
-];
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -112,31 +22,100 @@ app.use(methodOverride());
 app.use(morgan('common'));
 app.use(express.static('public/documentation.html'));
 
-// GET requests //////////////////////////////////
+let movies = [{
+        id: 1,
+        title: '12 Angry Men',
+        director: 'Sidney Lumet',
+        genre: ['Drama', 'Crime'],
+        year: '1957',
+        collection: null
+    },
+    {
+        id: 2,
+        title: 'Fight Club',
+        director: 'David Fincher',
+        genre: 'Drama',
+        year: '1999',
+        collection: null
+    },
+    {
+        id: 3,
+        title: 'American Pop',
+        director: 'Ralph Bakshi',
+        genre: ['Animation', 'Drama'],
+        year: '1981',
+        collection: null
+    },
+    {
+        id: 4,
+        title: 'The Wiz',
+        director: 'Sindey Lumet',
+        genre: ['Adventure', 'Family', 'Fantasy'],
+        year: '1978',
+        collection: null
+    },
+    {
+        id: 5,
+        title: 'Hook',
+        director: 'Steven Spielberg',
+        genre: ['Adventure', 'Comedy', 'Family'],
+        year: '1991',
+        collection: null
+    },
+    {
+        id: 6,
+        title: 'Dog Day Afternoon',
+        director: 'Sidney Lumet',
+        genre: ['Biography', 'Crime', 'Drama'],
+        year: '1975',
+        collection: null
+    },
+    {
+        id: 7,
+        title: 'Ready Player One',
+        director: 'Steven Spielberg',
+        genre: ['Action', 'Adventure', 'Sci-Fi'],
+        year: '2018',
+        collection: null
+    },
+    {
+        id: 8,
+        title: 'Rogue One: A Star Wars Story',
+        director: 'Gareth Edwards',
+        genre: ['Action', 'Adventure', 'Sci-Fi'],
+        year: '2016',
+        collection: 'Star Wars'
+    },
+    {
+        id: 9,
+        title: 'The Lord of the Rings: The Fellowship of the Ring',
+        director: 'Peter Jackson',
+        author: 'J.R.R Tolkien',
+        genre: ['Action', 'Adventure', 'Fantasy'],
+        year: '2001',
+        collection: 'The Lord of the Rings'
+    },
+    {
+        id: 10,
+        title: 'Carlito\'s Way',
+        director: 'Brian De Palma',
+        year: '1993',
+        genre: ['Crime', 'Drama', 'Thriller'],
+        collection: null
+    },
+];
+
+// GET Requests //////////////////////////////////////////////////
+
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix')
 });
-
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname });
 });
+// POST Requests //////////////////////////////////////////////////
 
-app.get('/movies', (req, res) => {
-    res.json(movies);
-});
 
-app.get('/movies/:titles', (req, res) => {
-    res.send('Movies by Title')
-});
-app.get('/movies/:titles/:directors', (req, res) => {
-    res.send('Movie Directors');
-});
-app.get('/movies/:titles/:directors/:genres', (req, res) => {
-    res.send('Movies by Genre');
-});
-app.get('/movies/:users', (req, res) => {
-    res.send('User Homepage');
-});
 
 
 // express error handling
@@ -145,7 +124,7 @@ app.use(function(err, req, res, next) {
         res.status(500).send('Try Again')
 })
 
-// listen for requests //////////////////////////////////
+// listen for requests //////////////////////////////////////////////////
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
