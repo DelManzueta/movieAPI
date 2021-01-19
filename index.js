5eaad1a70e0cb0da31c004268ab794706f859c51
Del <dmanzueta@gmail.com>
2021-01-19 18:09:02 -0500

    connect api and db

--- index.js
+++ index.js
@@ -21,10 +21,22 @@ const
     Genres = Models.Genre,
     Directors = Models.Director;
 
+/*
+    
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+ 
+  
+mongoose.connect('mongodb+srv://zueta:Trustno!123!@myflixdb.hsfga.mongodb.net/myFlixDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+*/
+
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common')); // log requests to server
 app.use(express.static('public'));
@@ -132,35 +144,7 @@ app.get('/login', function(req, res) {
 
 
 /* POST requests */
-/*
-app.post('/users', (req, res) => {
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username }) // search to see if a user with the requested username already exists
-        .then((user) => {
-            if (user) {
-                //if the user is found, send a response that it already exists
-                return res.status(400).send(req.body.Username + ' already exists');
-            } else {
-                Users
-                    .create({
-                        Username: req.body.Username,
-                        Password: hashedPassword,
-                        Email: req.body.Email,
-                        Birthday: req.body.Birthday
-                    })
-                    .then((user) => { res.status(201).json(user) })
-                    .catch((error) => {
-                        console.error(error);
-                        res.status(500).send('Error: ' + error);
-                    });
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
-        });
-});
-*/
+
 app.post('/users',
     // Validation logic here for request
     //you can either use a chain of methods like .not().isEmpty()
@@ -315,12 +299,6 @@ app.delete('/users/:Username/Favorites/:MovieID', passport.authenticate('jwt', {
 });
 
 
-// check the validation object for errors
-let errors = validationResult(req);
-
-if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-}
 
 // express error handling 
 app.use(function(err, req, res, next) {
