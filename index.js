17ad325c6499cac5326e5c040072b5d7d690c280
Del <dmanzueta@gmail.com>
2021-02-09 22:52:11 -0500

    updated index

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ const { check, validationResult } = require('express-validator');
 mongoose.connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() =>
-    console.log("Database Connected Successfully")
-).catch(err => console.log(err));
+});
 
 
 require('./passport')
