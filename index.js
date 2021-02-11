cd38d90f1d960ded96f316d3b93480154463c9e8
Del <dmanzueta@gmail.com>
2021-02-11 01:09:14 -0500

    added app.all to fix cors issue

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ app.use(cors({
     }
 }));
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
 
 app.get('/public', (res) => {
     res.sendFile('public/documentation.html', {
