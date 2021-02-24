1eb510119b127fb5fb702d11d38b279f42348dca
DelManzueta <dmanzueta@gmail.com>
2021-02-24 02:06:48 -0500

    *

--- index.js
+++ index.js
@@ -218,6 +218,6 @@ app.use((err, res) => {
 
  
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0/0', () => {
   console.log('Running the show on ' + PORT + '!');
 });
