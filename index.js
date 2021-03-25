d7e1e1f275c3307b8cc1dbb4e07d4e9607367c83
DelManzueta <dmanzueta@gmail.com>
2021-03-25 05:04:04 -0400

    working jwt

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ app.get('/', (req, res) => {
 
 
 // GET all movies
-app.get("/movies",
-  (req, res) => {
+app.get("/movies", (req, res) => {
     Movies.find().then((movies) => {
         res.status(201).json(movies);
     }).catch((err) => {
